dfceac9a17763f5a3ae24e4ce72a58e03aedab1f
Stennis1 <stennismith@gmail.com>
2025-05-19 19:38:23 +0000

    refactor: update dao functionalities

--- src/main/java/com/novatech/dao/TaskDAO.java
+++ src/main/java/com/novatech/dao/TaskDAO.java
@@ -1,4 +1,130 @@
 package com.novatech.dao;
 
+import com.novatech.model.Task;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TaskDAO {
+
+    // Initiate DB Connection
+    private final String URL = "jdbc:postgresql://localhost:5432/task_db";
+    private final String USER = "stennis";
+    private final String PASSWORD = "stennis12345";
+
+    // Load PostgreSQL driver
+    public TaskDAO() {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+
+    public void insertTask(Task task) {
+        String sql = "INSERT INTO task (title, description, due_date, status) VALUES (?, ?, ?, ?)";
+
+        try(Connection connection = getConnection();
+            PreparedStatement stmt = connection.prepareStatement(sql)) {
+
+            stmt.setString(1, task.getTitle());
+            stmt.setString(2, task.getDescription());
+            stmt.setDate(3, Date.valueOf(task.getDueDate()));
+            stmt.setString(3, task.getStatus());
+
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public List<Task> getAllTasks() {
+        List<Task> tasks = new ArrayList<>();
+        String sql = "SELECT * FROM task ORDER BY due_date ASC";
+
+        try(Connection connection = getConnection();
+        PreparedStatement stmt = connection.prepareStatement(sql);
+        ResultSet rs = stmt.executeQuery()) {
+
+        while (rs.next()) {
+            Task task = new Task();
+            task.setId(rs.getInt("id"));
+            task.setTitle(rs.getString("title"));
+            task.setDescription(rs.getString("description"));
+            task.setDueDate(rs.getDate("due_date").toLocalDate());
+            task.setStatus(rs.getString("status"));
+
+            tasks.add(task);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return tasks;
+    }
+
+    public Task getTaskById(int id) {
+        Task task = null;
+        String sql = "SELECT FROM task WHERE id = ?";
+
+        try(Connection connection = getConnection();
+        PreparedStatement stmt = connection.prepareStatement(sql)){
+
+        stmt.setInt(1, id);
+        ResultSet rs = stmt.executeQuery();
+
+        if (rs.next()) {
+            task = new Task();
+            task.setId(rs.getInt("id"));
+            task.setTitle(rs.getString("title"));
+            task.setDescription(rs.getString("description"));
+            task.setDueDate(rs.getDate("due_date").toLocalDate());
+            task.setStatus(rs.getString("status"));
+        }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return task;
+    }
+
+    public void updateTask(Task task) {
+        String sql = "UPDATE task SET title= ?, description = ?, due_date = ?, status = ? WHERE id = ?";
+
+        try (Connection connection = getConnection();
+             PreparedStatement stmt = connection.prepareStatement(sql)) {
+
+            stmt.setString(1, task.getTitle());
+            stmt.setString(2, task.getDescription());
+            stmt.setDate(3, Date.valueOf(task.getDueDate()));
+            stmt.setString(4, task.getStatus());
+            stmt.setInt(5, task.getId());
+
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteTask(int id) {
+        String sql = "DELETE FROM task WHERE id = ?";
+
+        try (Connection connection = getConnection();
+        PreparedStatement stmt = connection.prepareStatement(sql)) {
+
+        stmt.setInt(1, id);
+        stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
