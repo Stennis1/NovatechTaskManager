ee9a7d616921c3e1c344da4d04b99fa8ff7b23bd
Stennis1 <stennismith@gmail.com>
2025-05-19 17:30:35 +0000

    build: add Task model and dao

--- src/main/java/com/novatech/model/Task.java
+++ src/main/java/com/novatech/model/Task.java
@@ -26,4 +26,44 @@ public class Task {
         this.dueDate = dueDate;
         this.status = status;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
